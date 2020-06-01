a419fb4388505541ba53731fbabf6b733d32ac1f
Racs Ramás <cant0r.universal@gmail.com>
2020-06-01 13:34:20 +0200

    Added Basic Database Schema

---
+++ Server/Models/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Job
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public virtual Auto Auto { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public virtual Manager JobManager { get; set; }
+
+        [Required]
+        public virtual State WorkState { get; set; }
+
+        [Required]
+        public virtual ICollection<JobTechnician> JobTechnicians { get; set; }
+    }
+}
