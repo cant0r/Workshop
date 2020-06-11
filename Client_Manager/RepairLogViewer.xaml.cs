e03a47602bf5eaef442fa17a12bc92657ffa685e
Racs Tamás <cant0r.universal@gmail.com>
2020-06-11 14:11:56 +0200

    Implemented the repair log viewer window.

---
+++ Client_Manager/RepairLogViewer.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client_Manager
+{
+    /// <summary>
+    /// Interaction logic for RepairLogViewer.xaml
+    /// </summary>
+    public partial class RepairLogViewer : Window
+    {
+        public RepairLogViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
