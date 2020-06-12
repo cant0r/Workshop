bb023afbee3e5544a405722f9e112a8e6ae64787
Racs Tamás <cant0r.universal@gmail.com>
2020-06-12 17:21:29 +0200

    Implemented a clean up timer and made the buttons turn inside out upon clicking on them.

--- Client_Manager/Views/DatabaseView.xaml.cs
+++ Client_Manager/Views/DatabaseView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Client_Manager.CustomControls;
+using Client_Manager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Client_Manager
 {
@@ -20,10 +22,39 @@ namespace Client_Manager
     /// </summary>
     public partial class DatabaseView : Window
     {
-      
+        private Button activeButton;
+
+        private Manager theManager;
+
         public DatabaseView()
         {
             InitializeComponent();
+            theManager = Manager.GetInstance();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += (object sender, EventArgs args) =>
+            {
+                entryPanel.Children.Clear();
+                theManager.ParseDatabase();
+                
+            };
+            timer.Interval = TimeSpan.FromMinutes(5);
+            timer.Start();
+        }
+
+        private void MakeButtonActive(Button btn)
+        {
+            FlipColorsOfButton(activeButton);
+            activeButton = btn;
+            FlipColorsOfButton(activeButton);
+        }
+
+        private void FlipColorsOfButton(Button btn)
+        {
+            if (btn is null)
+                return;
+            Brush temp = btn.Background;
+            btn.Background = btn.Foreground;
+            btn.Foreground = temp;
         }
 
         private void backBtn_Click(object sender, RoutedEventArgs e)
@@ -34,6 +65,35 @@ namespace Client_Manager
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             DialogResult = true;
+        }
+
+        private void progressBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Justification for the Button casting: Only a single Button instance's click event is tied to this method.
+            MakeButtonActive((Button)sender);
+
+
+
+        }
+
+        private void closedBtn_Click(object sender, RoutedEventArgs e)
+        {          
+            MakeButtonActive((Button)sender);
+        }
+
+        private void techniciansBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MakeButtonActive((Button)sender);
+        }
+
+        private void clientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MakeButtonActive((Button)sender);
+        }
+
+        private void autosBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MakeButtonActive((Button)sender);
         }
     }
 }
