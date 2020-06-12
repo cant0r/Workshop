9364d519c4e4d26b143b708e1367665de9b18d5a
Racs Tamás <cant0r.universal@gmail.com>
2020-06-12 17:22:03 +0200

    Added a class to manage the retreived database objects.

---
+++ Client_Manager/Models/Manager.cs
@@ -0,0 +1,46 @@
+﻿using ModelProvider;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Windows.Automation;
+
+namespace Client_Manager.Models
+{
+    public sealed class Manager
+    {
+        public List<Auto> Autos { get; private set; }
+        public List<Client> Clients { get; private set; }
+        public List<Repair> Repairs { get; private set; }
+        public List<RepairLog> RepairLogs { get; private set; }
+        public List<Technician> Technicians { get; private set; }
+
+        private WorkshopClient workshopClient;
+
+        private static readonly Manager instance;
+
+
+        static Manager()
+        {
+            instance = new Manager(); 
+        }
+        private Manager() 
+        {
+            workshopClient = WorkshopClient.GetInstance();
+        }
+        public static Manager GetInstance()
+        {
+            return instance;
+        }
+
+        public void ParseDatabase()
+        {
+            Autos = workshopClient.RetrieveEntities<Auto>();
+            Clients = workshopClient.RetrieveEntities<Client>();
+            Repairs = workshopClient.RetrieveEntities<Repair>();
+            RepairLogs = workshopClient.RetrieveEntities<RepairLog>();
+            Technicians = workshopClient.RetrieveEntities<Technician>();
+        }
+
+    }
+}
