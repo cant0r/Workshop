805b8902c6f632250ffa399d1ee2c0659de1d30e
Racs Tamás <cant0r.universal@gmail.com>
2020-06-13 13:43:16 +0200

    Added a table that'll contain the necessary informations for implementing a pseuod-auth procedure of the users.

---
+++ ModelProvider/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModelProvider
+{
+    public class User
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
