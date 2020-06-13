eec72d82ac6edca868249dabb9753a0c04a94ade
Racs Tamás <cant0r.universal@gmail.com>
2020-06-13 19:53:38 +0200

    Implemented the repair entry uploader and updater methods

--- Client_Manager/Models/WorkshopClient.cs
+++ Client_Manager/Models/WorkshopClient.cs
@@ -17,11 +17,11 @@ namespace Client_Manager.Models
         private readonly Dictionary<Type, string> URIparts;
         private readonly string baseUri;
         private static bool disposed = false;
-        private WorkshopClient() 
+        private WorkshopClient()
         {
             httpClient = new HttpClient();
             baseUri = "https://localhost:5001/api/workshop/manager";
-            URIparts = new Dictionary<Type, string>() 
+            URIparts = new Dictionary<Type, string>()
             {
                 { typeof(Auto), "/autos" },
                 { typeof(Repair), "/repair" },
@@ -30,12 +30,12 @@ namespace Client_Manager.Models
                 { typeof(Client), "/clients" },
                 { typeof(Bonus), "/bonus" }
             };
-          
+
         }
         static WorkshopClient()
         {
             instance = new WorkshopClient();
-            
+
         }
 
         public static WorkshopClient GetInstance()
@@ -49,18 +49,55 @@ namespace Client_Manager.Models
             {
                 URIPart = URIparts[typeof(TEntity)];
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message, "NULL vagy kulcs nem létezik hiba", MessageBoxButton.OK);
                 return null;
             }
             var result = httpClient.GetAsync(baseUri + URIPart).Result;
-            if(!result.IsSuccessStatusCode)
+            if (!result.IsSuccessStatusCode)
             {
                 MessageBox.Show("There's no data available!");
             }
             var rawContent = result.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<TEntity>>(rawContent);
+            var parserSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, MaxDepth = null };
+            return JsonConvert.DeserializeObject<List<TEntity>>(rawContent, parserSettings);
+        }
+        public void UploadRepair(Repair repair)
+        {
+            string URIPart = URIparts[typeof(Repair)];
+            var parserSettings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                MaxDepth = null
+            };
+            var json = JsonConvert.SerializeObject(repair, parserSettings);
+            var rawData = new StringContent(json, Encoding.UTF8, "application/json");
+            MessageBox.Show(json);
+            var result = httpClient.PostAsync(baseUri + URIPart, rawData).Result;
+
+            if (!result.IsSuccessStatusCode)
+            {
+                MessageBox.Show(result.ReasonPhrase, result.StatusCode.ToString());
+            }
+        }
+        public void UploadUpdatedRepair(Repair repair)
+        {
+            string URIPart = URIparts[typeof(Repair)];
+            var parserSettings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                MaxDepth = null
+            };
+            var json = JsonConvert.SerializeObject(repair, parserSettings);
+            var rawData = new StringContent(json, Encoding.UTF8, "application/json");
+            MessageBox.Show(json);
+            var result = httpClient.PutAsync(baseUri + URIPart, rawData).Result;
+
+            if (!result.IsSuccessStatusCode)
+            {
+                MessageBox.Show(result.ReasonPhrase, result.StatusCode.ToString());
+            }
         }
 
         public void Dispose() => Dispose(true);
@@ -72,7 +109,7 @@ namespace Client_Manager.Models
 
             if (disposing)
             {
-                httpClient.Dispose();              
+                httpClient.Dispose();
             }
 
             disposed = true;
