11875358d4004d19a81919427a413813c351230b
Racs Tamás <cant0r.universal@gmail.com>
2020-06-14 09:51:58 +0200

    Fixed parsing errors related to repairs and also split up the ugly AddOrRemove method

--- Client_Manager/Views/RepairView.xaml.cs
+++ Client_Manager/Views/RepairView.xaml.cs
@@ -39,26 +39,23 @@ namespace Client_Manager.Views
 
         private void LoadRepair(Repair repair)
         {
-            //ITT BECRASHELT VALAHOGY
             theRepair = repair;
             realPriceLbl.Content = theRepair.Price;
             problemTbox.Text = theRepair.Description.ToString();
 
             long noBonusesPrice = theRepair.Price - theRepair?.Bonuses.Sum(b => b.Price) ?? 0;
             priceTbox.Text = noBonusesPrice.ToString();
-            
+
             foreach (Bonus b in theRepair.Bonuses)
             {
                 Label bonus = new Label();
                 bonus.Content = b.Name + " " + b.Price;
                 bonus.HorizontalContentAlignment = HorizontalAlignment.Center;
                 bonus.VerticalContentAlignment = VerticalAlignment.Center;
-                bonusStackPanel.Children.Add(bonus);
-
-
+        
                 foreach (CheckBox cb in bonusLbox.Children)
                 {
-                    if (cb.Name == b.Name)
+                    if (cb.Content.ToString().Contains(b.Name))
                     {
                         cb.IsChecked = true;
                         break;
@@ -79,22 +76,24 @@ namespace Client_Manager.Views
                 {
                     Label name = new Label();
                     name.Content = b.Name + " " + b.Price;
-                    AddOrRemoveBonus(name);
-                    CalculateActualPrice();
+                    AddBonus(name);
+                    RefreshClientBonuses();
+                    UpdateRealPrice();
                 };
                 cbox.Unchecked += (object sender, RoutedEventArgs args) =>
                 {
                     Label name = new Label();
                     name.Content = b.Name + " " + b.Price;
-                    AddOrRemoveBonus(name);
-                    CalculateActualPrice();
+                    RemoveBonus(name);
+                    RefreshClientBonuses();
+                    UpdateRealPrice();
                 };
                 bonusLbox.Children.Add(cbox);
             }
 
         }
 
-        private void CalculateActualPrice()
+        private void RefreshClientBonuses()
         {
             List<Bonus> selectedBonuses = new List<Bonus>();
 
@@ -113,28 +112,17 @@ namespace Client_Manager.Views
             theRepair.Bonuses = selectedBonuses;
         }
 
-        private void AddOrRemoveBonus(Label name)
+        private void AddBonus(Label name)
         {
+            bonusStackPanel.Children.Add(name);
+        }
+        private void RemoveBonus(Label name)
+        {
+            var b_temp = (from ctrl in bonusStackPanel.Children.OfType<UIElement>()
+                          where ((Label)ctrl).Content.ToString().Contains(name.Content.ToString())
+                          select ctrl).FirstOrDefault();
 
-            Action<Label, StackPanel> addOrRemove = (Label c, StackPanel sp) =>
-             {
-                 var b_temp = (from ctrl in sp.Children.OfType<UIElement>()
-                               where ((Label)ctrl).Content.ToString().Contains(c.Content.ToString())
-                               select ctrl).FirstOrDefault();
-                 if (b_temp != null)
-                 {
-                     sp.Children.Remove(b_temp);
-                 }
-                 else
-                 {
-                     sp.Children.Add(c);
-                 }
-                 //Triggering the textchanged event of priceTbox
-                 priceTbox.Text = priceTbox.Text;
-             };
-
-            addOrRemove(name, bonusStackPanel);
-
+            bonusStackPanel.Children.Remove(b_temp);
         }
 
 
@@ -159,11 +147,15 @@ namespace Client_Manager.Views
             DialogResult = true;
             Close();
         }
-
-        private void priceTbox_TextChanged(object sender, TextChangedEventArgs e)
+        private void UpdateRealPrice()
         {
             if (new RegistrationFormValidator().ValidateCostValue(priceTbox))
                 realPriceLbl.Content = long.Parse(Regex.Replace(priceTbox.Text, @"\s", "")) + (theRepair?.Bonuses.Sum(b => b.Price) ?? 0);
+        }
+
+        private void priceTbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateRealPrice();
         }
     }
 }
